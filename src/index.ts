8eb181efbf6322ace46477756ee60f2f0a51d924
Hesedel Pajaron <pajaron@mediamarktsaturn.com>
2021-03-11 14:11:09 +0100

    feat(styled): exposed styled components for custom styling

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 import { Carousel } from './components/Carousel/index'
 export const Slider = Carousel
+export { StyledCarousel, StyledSlider, StyledUl } from './components/Carousel/Carousel.styled'
+export { StyledSlide } from './components/Slide/Slide.styled'
+export { StyledNavWrapper, StyledArrow } from './components/NavArrow/NavArrow.styled'
