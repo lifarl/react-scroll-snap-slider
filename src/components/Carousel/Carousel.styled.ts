a8fef055cb06f3b753dd22e0056c4d9bfee5b379
Hashem Khalifa <hashem.khalifa@gmail.com>
2021-09-27 11:00:40 +0200

    Update Carousel.styled.ts

--- src/components/Carousel/Carousel.styled.ts
+++ src/components/Carousel/Carousel.styled.ts
@@ -16,7 +16,7 @@ export const StyledSlider = styled.div`
   scrollbar-width: none;
   scroll-snap-type: x mandatory;
   scroll-behavior: smooth;
-  overflow-x: scroll;
+  overflow-x: hidden;
   width: 100%;
 `
 
